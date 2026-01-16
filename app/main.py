7ba82dc276d8fae8dd7bbad395dc574b6ae6bbac
Your Name <you@example.com>
2026-01-16 17:07:25 +0200

    correction2

--- app/main.py
+++ app/main.py
@@ -13,16 +13,12 @@ def create_person_list(people: list[dict]) -> list[Person]:
     created_list = [Person(d["name"], d["age"]) for d in people]
 
     for inst, person_dict in zip(created_list, people):
-        wife_name = person_dict.get("wife")
-        if wife_name:
-            wife_obj = Person.people.get(wife_name)
-            if wife_obj:
-                inst.wife = wife_obj
+        if (wife_name := person_dict.get("wife")) and \
+           (wife_obj := Person.people.get(wife_name)):
+            inst.wife = wife_obj
 
-        husband_name = person_dict.get("husband")
-        if husband_name:
-            husband_obj = Person.people.get(husband_name)
-            if husband_obj:
-                inst.husband = husband_obj
+        if (husband_name := person_dict.get("husband")) and \
+           (husband_obj := Person.people.get(husband_name)):
+            inst.husband = husband_obj
 
     return created_list
